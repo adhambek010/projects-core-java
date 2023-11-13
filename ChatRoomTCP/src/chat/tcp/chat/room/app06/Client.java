ae819c15ffc298dff0ceb5b1758e6492441fda9e
adhambek010 <adhamsalaydinov5@gmail.com>
2023-11-13 12:06:37 +0500

    Chat room TCP

---
+++ ChatRoomTCP/src/chat/tcp/chat/room/app06/Client.java
@@ -0,0 +1,70 @@
+package chat.tcp.chat.room.app06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client implements Runnable{
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
+    public static void main(String[] args) {
+        Client client1 = new Client();
+        client1.run();
+    }
+    @Override
+    public void run() {
+        try {
+            client = new Socket("127.0.0.1",9999);
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            InputHandler inHandler = new InputHandler();
+            Thread t = new Thread(inHandler);
+            t.start();
+            String inMessage;
+            while((inMessage = in.readLine()) != null){
+                System.out.println(inMessage);
+            }
+        }catch (IOException e){
+            shutDown();
+        }
+    }
+    public void shutDown(){
+        try{
+            in.close();
+            out.close();
+            if(!client.isClosed()){
+                client.close();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    class InputHandler implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while(!done){
+                    String message = inReader.readLine();
+                    if (message.equals("/quit")) {
+                        out.println(message);
+                        done = true;  // Set the flag to terminate the main thread as well
+                        inReader.close();
+                        shutDown();
+                    } else {
+                        out.println(message);
+                    }
+                }
+            }catch (IOException e){
+                shutDown();
+            }
+        }
+    }
+
+}
